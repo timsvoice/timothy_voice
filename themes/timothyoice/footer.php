d2e9d373b10667f7841eaffc5fe84a8d96330867
Timothy Voice <timsethvoice@gmail.com>
2014-03-12 22:25:05 -0400

    removed submodule

---
+++ themes/timothyoice/footer.php
@@ -0,0 +1,18 @@
+  <script src="<?php bloginfo('template_url'); ?>/bower_components/jquery/jquery.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/bower_components/foundation/js/foundation.min.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/bower_components/foundation/js/foundation/foundation.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/bower_components/foundation/js/foundation/foundation.topbar.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/bower_components/foundation/js/app.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/bower_components/foundation/js/foundation/foundation.dropdown.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/bower_components/foundation/js/foundation/foundation.accordion.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/javascript.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/bower_components/foundation/js/foundation/foundation.magellan.js"></script>
+
+  
+  
+  <script src="<?php bloginfo('template_url'); ?>/bower_components/foundation/js/foundation/foundation.reveal.js"></script>
+
+  <script> $(document).foundation(); </script>
+
+  </body>
+</html>
